038438148f2193e1b57465c6c4d9084801ffec14
PatriceHANNE <patrice.hanne@gmail.com>
2023-12-18 15:37:58 +0100

    FIX : BDD

--- Program.cs
+++ Program.cs
@@ -8,10 +8,10 @@ namespace ConsolAppMySQL
         static void Main(string[] args)
         {
             // Connexion à la BDD
-            string cs = @"server=localhost;userid=root;password=;database=tutoMySql";
+            string cs = @"server=localhost;userid=root;password=;database=tuto";
             using var con = new MySqlConnection(cs);
             con.Open();
-            Console.WriteLine($"Connecté à la BDD");
+            Console.WriteLine("Connecté à la BDD !");
 
             // SELECT
             var stm = "SELECT VERSION()";
@@ -22,9 +22,20 @@ namespace ConsolAppMySQL
             // INSERT
             using var cmd2 = new MySqlCommand();
             cmd2.Connection = con;
-            cmd2.CommandText = "INSERT INTO listeTutos(name, price) VALUES('WPF' , 49)";
+            cmd2.CommandText = "INSERT INTO listetutos(name, price) VALUES('WEB' , 79)";
             cmd2.ExecuteNonQuery();
             Console.WriteLine("Une ligne à été ajoutée");
+
+            // SELECT TUTOS
+            string sql = "SELECT name, price FROM listetutos";
+            using var cmd3 = new MySqlCommand(sql, con);
+            using MySqlDataReader rdr = cmd3.ExecuteReader();
+
+            // Boucle sur les données pour les afficher
+            while (rdr.Read())
+            {
+                Console.WriteLine(rdr[0] + " | " + rdr[1] + " EUR");
+            }
         }
 
     }
